48a794257db812a26c8a2df5f85b755a1aa7be2b
ekalaiv <admin@ekalaiv.com>
2022-05-05 17:16:57 +0530

    main utils

---
+++ lib/auth.d.ts
@@ -0,0 +1,7 @@
+export declare const loginSchema: {
+    username: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+};
+export declare const emailSchema: {
+    email: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+};
