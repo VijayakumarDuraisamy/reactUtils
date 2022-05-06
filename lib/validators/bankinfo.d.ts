677697c63dc1b9bfc24b5aeba8b50eb6b4a2cb3d
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-05-06 15:55:08 +0530

    bank validation updated

---
+++ lib/validators/bankinfo.d.ts
@@ -0,0 +1,8 @@
+export declare const bankSchema: {
+    ifsc: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    bankaccountno: import("yup/lib/number").RequiredNumberSchema<number | undefined, import("yup/lib/types").AnyObject>;
+    bankname: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    address: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    branch: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    bankcode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+};
