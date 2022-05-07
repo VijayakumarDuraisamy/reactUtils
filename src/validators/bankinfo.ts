e6a25adf3c843d9a06d870f18f2b7061bb0ca9b7
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-05-07 16:04:57 +0530

    ifsc regex updated

--- src/validators/bankinfo.ts
+++ src/validators/bankinfo.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const bankSchema = {
-    bifsc: yup.string().required("ifsc field is required").matches(/^.*((?=.*[A-Z]){4})0((?=.*[A-Z0-9]){6}).*$/, "enter valid ifsc number"),
+    bifsc: yup.string().required("ifsc field is required").matches(/^.*[^\s]{4}\d{7}.*$/, "enter valid ifsc number"),
     bankaccountno: yup.number().required(REQUIRED_TEXT).min(9, "invalid number").max(18, "invalid number"),
     bname: yup.string().required(REQUIRED_TEXT),
     compcode: yup.string().required(REQUIRED_TEXT),
