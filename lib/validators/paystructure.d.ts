eedfca7ce3024bd1c383cb0b384f933ee67a5444
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-05-18 19:54:11 +0530

    paystructre updated

--- lib/validators/paystructure.d.ts
+++ lib/validators/paystructure.d.ts
@@ -5,3 +5,9 @@ export declare const paySchema: {
     pickfrom: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     formula: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
+export declare const emppayinfoSchema: {
+    paycode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    paydesc: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    preamount: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    amount: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+};
