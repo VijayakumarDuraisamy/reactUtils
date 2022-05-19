3dcfb0664e871f503e48773a815b2ba9ad591c34
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-05-19 23:10:16 +0530

    update

--- lib/validators/attentry.d.ts
+++ lib/validators/attentry.d.ts
@@ -1,7 +1,6 @@
 export declare const AttdnceEntrySchema: {
     docdate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     idcard: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    present: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     shift: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     temp: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     lop: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
--- src/validators/attentry.ts
+++ src/validators/attentry.ts
@@ -4,7 +4,6 @@ import { REQUIRED_TEXT } from '../config/validators'
 export const AttdnceEntrySchema = {
   docdate: yup.string().required(REQUIRED_TEXT),
   idcard: yup.string().required(REQUIRED_TEXT),
-  present: yup.string().required(REQUIRED_TEXT),
   shift: yup.string().required(REQUIRED_TEXT),
   temp: yup.string().required(REQUIRED_TEXT),
   lop: yup.string().required(REQUIRED_TEXT),
