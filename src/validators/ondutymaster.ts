cdfe13f7803933e787342315043018ee1cc3a1e9
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-05-27 19:01:48 +0530

    duty schema added

---
+++ src/validators/ondutymaster.ts
@@ -0,0 +1,9 @@
+import * as yup from 'yup'
+import { REQUIRED_TEXT } from '../config/validators'
+
+export const ondutymasterSchema = {
+  finyear: yup.string().required(REQUIRED_TEXT),
+  compname: yup.string().required(REQUIRED_TEXT),
+  docid: yup.string().required(REQUIRED_TEXT),
+  docdate: yup.string().required(REQUIRED_TEXT),
+  }
