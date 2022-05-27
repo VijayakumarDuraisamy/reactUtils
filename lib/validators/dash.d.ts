df13c6d91412e8ef08cd88d60f56058e94d379ad
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-05-27 14:12:11 +0530

    dashboard schema changes

--- lib/validators/dash.d.ts
+++ lib/validators/dash.d.ts
@@ -1,5 +1,4 @@
 export declare const dashSchema: {
-    hrdbrvid: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     docdate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     ptransaction: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     docid: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
--- src/validators/dash.ts
+++ src/validators/dash.ts
@@ -2,9 +2,8 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const dashSchema = {
-  hrdbrvid: yup.string().required(REQUIRED_TEXT),
   docdate: yup.string().required(REQUIRED_TEXT),
   ptransaction: yup.string().required(REQUIRED_TEXT),
- docid: yup.string().required(REQUIRED_TEXT),
+  docid: yup.string().required(REQUIRED_TEXT),
      
 }
