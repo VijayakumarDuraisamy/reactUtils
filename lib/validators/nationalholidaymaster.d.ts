621e885b2f2b4d0981beaceda8fc4659d7306010
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-06-14 17:15:02 +0530

    national holiday updated

--- lib/validators/nationalholidaymaster.d.ts
+++ lib/validators/nationalholidaymaster.d.ts
@@ -1,5 +1,3 @@
 export declare const holidaymasterSchema: {
-    finyear: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    compcode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     compname: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
