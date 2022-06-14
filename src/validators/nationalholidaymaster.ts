2665f9238939ad96c84e37da4aba9cc3e71c69b7
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-06-14 17:14:20 +0530

    national holiday updated

--- src/validators/nationalholidaymaster.ts
+++ src/validators/nationalholidaymaster.ts
@@ -2,7 +2,5 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const holidaymasterSchema = {
-    finyear: yup.string().required(REQUIRED_TEXT),
-    compcode: yup.string().required(REQUIRED_TEXT),
     compname: yup.string().required(REQUIRED_TEXT),
 }
