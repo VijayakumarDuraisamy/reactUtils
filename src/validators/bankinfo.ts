7d01250af17d2ee24746e2f64b4687a3543a1d5e
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-06-30 15:49:26 +0530

    lenght added

--- src/validators/bankinfo.ts
+++ src/validators/bankinfo.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 import { REQUIRED_TEXT } from '../config/validators';
 export var bankSchema = {
-    bifsc: yup.string().required("ifsc field is required").matches(/^[A-Z]{4}0[A-Z0-9]{6}$/, "enter valid ifsc number").length(11, "Must - First 4 chars alphabets 5th char zero and last 6 chars alphanumeric"),
+    bifsc: yup.string().required("ifsc field is required").matches(/^[A-Z]{4}0[A-Z0-9]{6}$/, "enter valid ifsc number").length(11, "Must - First 4 chars alphabets 5th char zero and last 6 chars alphanumeric").min(11, "Must - First 4 chars alphabets 5th char zero and last 6 chars alphanumeric").max(11, "Must - First 4 chars alphabets 5th char zero and last 6 chars alphanumeric"),
     bname: yup.string().required(REQUIRED_TEXT),
     compcode: yup.string().required(REQUIRED_TEXT),
     compname: yup.string().required(REQUIRED_TEXT),
