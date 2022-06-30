b415753ddb7c50d3966be1f8025ce42bd7043448
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-06-30 12:40:29 +0530

    fixes

--- lib/validators/secmas.d.ts
+++ lib/validators/secmas.d.ts
@@ -1,6 +1,6 @@
 export declare var secmasSchema: {
     docdate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    compcode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    finyr: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    tcompcode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    tfinyear: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     shift: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/secmas.ts
+++ src/validators/secmas.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 import { REQUIRED_TEXT } from '../config/validators';
 export var secmasSchema = {
     docdate: yup.string().required(REQUIRED_TEXT),
-    compcode: yup.string().required(REQUIRED_TEXT),
-    finyr: yup.string().required(REQUIRED_TEXT),
+    tcompcode: yup.string().required(REQUIRED_TEXT),
+    tfinyear: yup.string().required(REQUIRED_TEXT),
     shift: yup.string().required(REQUIRED_TEXT),
 };
