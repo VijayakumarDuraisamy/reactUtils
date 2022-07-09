d644d45fe4b051ae57821622d66ac4feb69b0d68
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-07-09 11:22:38 +0530

    fixes

--- lib/validators/emppayinfo.d.ts
+++ lib/validators/emppayinfo.d.ts
@@ -1,6 +1,4 @@
 export declare const emppayinfoSchema: {
     paycode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     paydesc: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    preamount: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    incamount: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
