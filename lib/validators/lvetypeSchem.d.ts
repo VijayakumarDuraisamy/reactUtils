971b38dec02b7991bad547c4053a72305de41fc8
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-07-12 16:07:15 +0530

    fixes

--- lib/validators/lvetypeSchem.d.ts
+++ lib/validators/lvetypeSchem.d.ts
@@ -1,4 +1,3 @@
 export declare const lvetypeSchema: {
     leavetype_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    allow_override: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/lvetypeSchem.ts
+++ src/validators/lvetypeSchem.ts
@@ -3,5 +3,5 @@ import { REQUIRED_TEXT } from '../config/validators'
 
 export const lvetypeSchema = {
     leavetype_name: yup.string().required(REQUIRED_TEXT),
-    allow_override: yup.string().required(REQUIRED_TEXT)
+    //allow_override: yup.string().required(REQUIRED_TEXT)
 }
