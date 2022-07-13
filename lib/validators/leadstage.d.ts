a30da802030f058c82f93957522ae546c0efb338
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-07-13 13:10:59 +0530

    leadstage

---
+++ lib/validators/leadstage.d.ts
@@ -0,0 +1,5 @@
+export declare const leadstageSchema: {
+    stage_level: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    stage_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    stage_description: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+};
---
+++ src/validators/leadstage.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup'
+import { REQUIRED_TEXT } from '../config/validators'
+
+export const leadstageSchema = {
+  stage_level: yup.string().required(REQUIRED_TEXT),
+  stage_name: yup.string().required(REQUIRED_TEXT),
+  stage_description: yup.string().required(REQUIRED_TEXT),
+  }
