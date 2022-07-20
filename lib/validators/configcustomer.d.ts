a1a5bcd186c85fa80e1d112ca1728d425fdcb301
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-07-20 16:02:55 +0530

    changes applied

--- lib/validators/configcustomer.d.ts
+++ lib/validators/configcustomer.d.ts
@@ -2,6 +2,7 @@ export namespace configcustomerSchema {
     const customer_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     const organization_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     const mobile_no: any;
-    const email: any;
-    const website: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    const email: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const website: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
 }
+import * as yup from "yup";
