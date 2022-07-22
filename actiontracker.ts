80726ecb459bf1ac96af4a7a67d203a8e99c183a
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-07-22 15:28:33 +0530

    fixes

---
+++ actiontracker.ts
@@ -0,0 +1,11 @@
+import * as yup from 'yup'
+import { REQUIRED_TEXT } from '../config/validators'
+
+export const actiontrackerSchema = {
+  item: yup.string().required(REQUIRED_TEXT),
+  start_date: yup.string().required(REQUIRED_TEXT),
+  exp_end_date: yup.string().required(REQUIRED_TEXT),
+  assigned_to_tracker: yup.string().required(REQUIRED_TEXT),
+  status: yup.string().required(REQUIRED_TEXT),
+
+  }
