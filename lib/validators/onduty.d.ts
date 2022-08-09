2c95bc620a9fccfa711662de4e2150ae4117613f
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-09 14:24:15 +0530

    fixes

--- lib/validators/onduty.d.ts
+++ lib/validators/onduty.d.ts
@@ -1,9 +1,6 @@
 export declare const ondutySchema: {
-    empname: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     idcard: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     paycat: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    odate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    oday: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     ohday: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     thrs: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
