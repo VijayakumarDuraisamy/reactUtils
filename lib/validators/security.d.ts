66a6c1eb99292ca7616cb433460826f2768537d3
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-10 23:32:33 +0530

    fixes

--- lib/validators/security.d.ts
+++ lib/validators/security.d.ts
@@ -1,8 +1,4 @@
 export declare const secSchema: {
     idcardno: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    alndate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    aintime: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    aoutdate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    aouttime: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     thrs: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/security.ts
+++ src/validators/security.ts
@@ -3,9 +3,9 @@ import { REQUIRED_TEXT } from '../config/validators'
 
 export const secSchema = {
   idcardno: yup.string().required(REQUIRED_TEXT),
-  alndate: yup.string().required(REQUIRED_TEXT),
-  aintime: yup.string().required(REQUIRED_TEXT),
-  aoutdate: yup.string().required(REQUIRED_TEXT),
-  aouttime: yup.string().required(REQUIRED_TEXT),
+  //alndate: yup.string().required(REQUIRED_TEXT),
+  //aintime: yup.string().required(REQUIRED_TEXT),
+  //aoutdate: yup.string().required(REQUIRED_TEXT),
+  //aouttime: yup.string().required(REQUIRED_TEXT),
   thrs: yup.string().required(REQUIRED_TEXT),
   }
