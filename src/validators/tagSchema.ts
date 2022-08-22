a5fb8efd5958b51814401573113d52f484109772
Gokulraj <gokulece888@yahoo.com>
2022-08-22 19:37:42 +0530

    CRM validation updated

--- src/validators/tagSchema.ts
+++ src/validators/tagSchema.ts
@@ -1,6 +1,11 @@
 import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
+import {LIMIT_TEXT} from '../config/validators'
+import {DESCRIPTION_TEXT} from '../config/validators'
+import {ALPHA_TEXT} from '../config/validators'
+
 
 export const tagSchema = {
-  tag_name: yup.string().trim().max(50).required(REQUIRED_TEXT),
-  }
+  tag_name: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/[abcdefghijklmnopqrstuvwxyz]+/ , (ALPHA_TEXT)),
+  tag_description:yup.string().trim().max(250,(DESCRIPTION_TEXT)).matches(/[abcdefghijklmnopqrstuvwxyz]+/ , (ALPHA_TEXT)),
+ }
