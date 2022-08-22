9d8b0d5923d0732c779f22c566b097c0b978a6d2
Gokulraj <gokulece888@yahoo.com>
2022-08-22 10:46:10 +0530

    Id Added

--- lib/validators/addlead.d.ts
+++ lib/validators/addlead.d.ts
@@ -1,4 +1,5 @@
 export declare const addLeadSchema: {
+    lead_id: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     lead_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     customer_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     tag: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
