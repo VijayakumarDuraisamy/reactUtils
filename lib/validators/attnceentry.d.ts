c1b84d66fec584a6c399fb97451b45d76c6b19d2
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-23 23:32:45 +0530

    fixes

--- lib/validators/attnceentry.d.ts
+++ lib/validators/attnceentry.d.ts
@@ -1,3 +1,3 @@
 export declare const AttdnceMasterSchema: {
-    docdate1: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    docdate1: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/attnceentry.ts
+++ src/validators/attnceentry.ts
@@ -3,5 +3,5 @@ import { REQUIRED_TEXT } from '../config/validators'
 
 export const AttdnceMasterSchema = {
   //compcode: yup.string().required(REQUIRED_TEXT),
-  docdate1: yup.string().required(REQUIRED_TEXT),
+  docdate1: yup.date().required(REQUIRED_TEXT),
   }
