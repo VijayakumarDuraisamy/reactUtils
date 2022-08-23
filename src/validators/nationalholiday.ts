9a1e8f77c5f51755e03c4b92cdf4993384487121
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-23 23:40:20 +0530

    fixes

--- src/validators/nationalholiday.ts
+++ src/validators/nationalholiday.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const holidaySchema = {
-    nhdate: yup.string().required(REQUIRED_TEXT),
+    nhdate: yup.date().required(REQUIRED_TEXT),
     reason: yup.string().required(REQUIRED_TEXT),
     holcat: yup.string().required(REQUIRED_TEXT),
     //appldetails: yup.string().required(REQUIRED_TEXT),
