1167fd0ed5d39c703bb09eb73dec056aed2b2d83
Gokulraj <gokulece888@yahoo.com>
2022-08-23 13:25:40 +0530

    CRM Validation Updated

--- lib/validators/individualcustomer.d.ts
+++ lib/validators/individualcustomer.d.ts
@@ -1,7 +1,13 @@
 export namespace individualcustomerSchema {
     const customer_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    const mobile_no: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
-    const email: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
-    const website: yup.StringSchema<string | null | undefined, import("yup/lib/types").AnyObject, string | null | undefined>;
+    const job_position: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const address: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const pan: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const mobile_no: import("yup/lib/number").RequiredNumberSchema<number | undefined, import("yup/lib/types").AnyObject>;
+    const city: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const state: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const email: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    const website: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    const pincode: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
 }
 import * as yup from "yup";
--- lib/validators/organisationcustomer.d.ts
+++ lib/validators/organisationcustomer.d.ts
@@ -1,7 +1,14 @@
 export namespace configcustomerSchema {
     const organization_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    const mobile_no: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
-    const email: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
-    const website: yup.StringSchema<string | null | undefined, import("yup/lib/types").AnyObject, string | null | undefined>;
+    const industry_type: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const address: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const pan: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const mobile_no: import("yup/lib/number").RequiredNumberSchema<number | undefined, import("yup/lib/types").AnyObject>;
+    const city: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const state: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const email: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    const website: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    const pincode: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
+    const gst: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
 }
 import * as yup from "yup";
