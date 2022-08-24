cb00edde51ed0d304dc3a8b114b68d62c3fa1e84
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-24 19:31:59 +0530

    date fix

--- lib/validators/nationalholiday.d.ts
+++ lib/validators/nationalholiday.d.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 export declare const holidaySchema: {
-    nhdate: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
+    nhdate: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     reason: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     holcat: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     notes: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
