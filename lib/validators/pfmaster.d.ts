1a31380df307e5dc806a5fee561dcac39b140ed0
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-25 00:38:58 +0530

    fixes

--- lib/validators/pfmaster.d.ts
+++ lib/validators/pfmaster.d.ts
@@ -1,1 +1,3 @@
-export declare const pfmasSchema: {};
+export declare const pfmasSchema: {
+    paycode: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+};
--- src/validators/otmasschema.ts
+++ src/validators/otmasschema.ts
@@ -2,6 +2,6 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const otmasSchema = {
-  docdate: yup.string().required(REQUIRED_TEXT),
+  docdate: yup.date().required(REQUIRED_TEXT).max("2500-00-00", "Invalid Date"),
   
 }
