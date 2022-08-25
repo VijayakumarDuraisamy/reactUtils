96f506ee3d0ecc09f0c9952842e9212eb99b2479
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-25 15:48:40 +0530

    fixes

--- src/validators/dash.ts
+++ src/validators/dash.ts
@@ -4,5 +4,5 @@ import { REQUIRED_TEXT } from '../config/validators'
 export const dashSchema = {
   docdate: yup.date().required(REQUIRED_TEXT).max("2500-00-00", "Invalid Date"),
   ptransaction: yup.string().required(REQUIRED_TEXT),
-       
+      
 }
