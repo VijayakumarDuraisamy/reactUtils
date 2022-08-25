3e825574da762183d6a73a8c56c8cf09a12a7dd2
Gokulraj <gokulece888@yahoo.com>
2022-08-25 11:25:57 +0530

    tag removed

--- lib/validators/addlead.d.ts
+++ lib/validators/addlead.d.ts
@@ -1,6 +1,5 @@
 export declare const addLeadSchema: {
     lead_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     customer_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    tag: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     lead_assigned_to: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/addlead.ts
+++ src/validators/addlead.ts
@@ -4,6 +4,5 @@ import { REQUIRED_TEXT } from '../config/validators'
 export const addLeadSchema = {
     lead_name: yup.string().trim().max(50).required(REQUIRED_TEXT),
     customer_name: yup.string().trim().max(50).required(REQUIRED_TEXT),
-    tag: yup.string().trim().required(REQUIRED_TEXT),
     lead_assigned_to: yup.string().trim().max(50).required(REQUIRED_TEXT),
 }
