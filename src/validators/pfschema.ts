da405d2007a3fd89f7f321e9e60c499f066f36d8
VijayakumarDuraisamy <vijayakumar.duraisamy@ekalaiv.com>
2022-08-28 11:06:28 +0530

    fixes number with decimals

--- src/validators/pfschema.ts
+++ src/validators/pfschema.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const pfSchema = {
-    fromvalue: yup.string().required(REQUIRED_TEXT).matches(/^[0-9]+$/, 'Enter numbers only'),
-    tovalue: yup.string().required(REQUIRED_TEXT).matches(/^[0-9]+$/, 'Enter numbers only'),
+    fromvalue: yup.string().required(REQUIRED_TEXT).matches(/^[+-]?(\d*\.)?\d+$/, 'Enter numbers only'),
+    tovalue: yup.string().required(REQUIRED_TEXT).matches(/^[+-]?(\d*\.)?\d+$/, 'Enter numbers only'),
    
 }
