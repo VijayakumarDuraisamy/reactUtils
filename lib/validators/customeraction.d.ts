4aec7cca1aa9195fe2cb65e2900ad876a15d9a62
unknown <dhinesh.sivakumar@ekalaiv.com>
2022-08-30 07:02:45 +0530

    action status - action level validation added

--- lib/validators/customeraction.d.ts
+++ lib/validators/customeraction.d.ts
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 export declare const customerActionSchema: {
+    action_level: import("yup/lib/number").RequiredNumberSchema<number | undefined, import("yup/lib/types").AnyObject>;
     action_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     action_description: yup.StringSchema<string | undefined, import("yup/lib/types").AnyObject, string | undefined>;
 };
--- src/validators/customeraction.ts
+++ src/validators/customeraction.ts
@@ -1,10 +1,11 @@
 import * as yup from 'yup'
-import { REQUIRED_TEXT } from '../config/validators'
+import { MINMAX_TEXT, NUM_TEXT, REQUIRED_TEXT } from '../config/validators'
 import {LIMIT_TEXT} from '../config/validators'
 import {DESCRIPTION_TEXT} from '../config/validators'
 import {ALPHA_TEXT} from '../config/validators'
 
 export const customerActionSchema = {
+  action_level: yup.number().typeError(NUM_TEXT).min(1,(MINMAX_TEXT)).max(5,(MINMAX_TEXT)).required(REQUIRED_TEXT),
   action_name: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
   action_description:yup.string().max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/, (ALPHA_TEXT)),
  }
