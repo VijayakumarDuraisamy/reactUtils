723cc6764fd8925a1eff2e2bd144e6c53b1e61f3
Gokulraj <gokulece888@yahoo.com>
2022-09-06 12:34:07 +0530

    bankinfo updated

--- src/validators/bankinfo.ts
+++ src/validators/bankinfo.ts
@@ -2,11 +2,11 @@ import * as yup from 'yup';
 import { REQUIRED_TEXT } from '../config/validators';
 export var bankSchema = {
     bifsc: yup.string().required("ifsc field is required").matches(/^[A-Z]{4}0[A-Z0-9]{6}$/, "Must - First 4 chars alphabets 5th char zero and last 6 chars alphanumeric"),
-    bname: yup.string().required(REQUIRED_TEXT).matches(/^[A-Za-z]+$/, "Enter Valid name"),
+    bname: yup.string().required(REQUIRED_TEXT).matches(/^[A-Za-z\\s]+$/, "Enter Valid name"),
     //compcode: yup.string().required(REQUIRED_TEXT),
     //compname: yup.string().required(REQUIRED_TEXT),
     badd: yup.string().required(REQUIRED_TEXT),
-    bbranch: yup.string().required(REQUIRED_TEXT).matches(/^[A-Za-z]+$/, "Special charectors, Numbers not accepted"),
+    bbranch: yup.string().required(REQUIRED_TEXT).matches(/^[A-Za-z\\s]+$/, "Special charectors, Numbers not accepted"),
     bcode: yup.string().required(REQUIRED_TEXT).matches(/^[A-Za-z]+$/, "Special charectors, Numbers not accepted"),
     active: yup.string().required(REQUIRED_TEXT),
 };
