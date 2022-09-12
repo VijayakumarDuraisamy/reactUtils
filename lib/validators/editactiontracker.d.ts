f10c5274b3e1b9377a407257095e0c71b64e655a
Gokulraj <gokulece888@yahoo.com>
2022-09-12 14:07:22 +0530

    updated

--- lib/validators/editactiontracker.d.ts
+++ lib/validators/editactiontracker.d.ts
@@ -1,4 +1,4 @@
-export declare const editactontrackerSchema: {
+export declare const editactiontrackerSchema: {
     item: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     lead_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     start_date: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
--- src/validators/editactiontracker.ts
+++ src/validators/editactiontracker.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 import { DESCRIPTION_TEXT,REQUIRED_TEXT } from '../config/validators'
 
-export const editactontrackerSchema = {
+export const editactiontrackerSchema = {
   item: yup.string().trim().max(250,(DESCRIPTION_TEXT)).required(REQUIRED_TEXT),
   lead_name: yup.string().trim().max(250,(DESCRIPTION_TEXT)).required(REQUIRED_TEXT),
   start_date: yup.date().required(REQUIRED_TEXT).min("2022-00-00", "Invalid Date"),
