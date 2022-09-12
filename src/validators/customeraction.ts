a159fe527cfde43c1dcbc66b9c647b831c4ef0e4
Gokulraj <gokulece888@yahoo.com>
2022-09-12 15:04:23 +0530

    Updated max action Status

--- src/validators/customeraction.ts
+++ src/validators/customeraction.ts
@@ -5,7 +5,7 @@ import {DESCRIPTION_TEXT} from '../config/validators'
 import {ALPHA_TEXT} from '../config/validators'
 
 export const customerActionSchema = {
-  action_level: yup.number().typeError(NUM_TEXT).min(1,(MINMAX_TEXT)).max(5,(MINMAX_TEXT)).required(REQUIRED_TEXT),
+  action_level: yup.number().typeError(NUM_TEXT).min(1,(MINMAX_TEXT)).max(6,(MINMAX_TEXT)).required(REQUIRED_TEXT),
   action_name: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
   action_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/, (ALPHA_TEXT)),
  }
