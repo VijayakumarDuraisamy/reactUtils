8081491c1cf9f5dfe53343008fa0b8399fa1176e
Gokulraj <gokulece888@yahoo.com>
2022-09-12 15:44:45 +0530

    Update login crm

--- lib/validators/login.d.ts
+++ lib/validators/login.d.ts
@@ -1,5 +1,5 @@
 export declare const loginSchema: {
-    username: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
+    username: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     password: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     email: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/login.ts
+++ src/validators/login.ts
@@ -3,7 +3,7 @@ import { REQUIRED_TEXT } from '../config/validators'
 import { EMAIL_TEXT } from '../config/validators'
 
 export const loginSchema = {
-    username: yup.date().required(REQUIRED_TEXT),
+    username: yup.string().required(REQUIRED_TEXT),
     password: yup.string().required(REQUIRED_TEXT),
     email: yup.string().trim().email(EMAIL_TEXT).required(REQUIRED_TEXT),
    
