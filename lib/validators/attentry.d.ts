471fbb71b042e5bcbf6df5461fc7799237c2e085
Gokulraj <gokulece888@yahoo.com>
2022-09-12 16:35:34 +0530

    attentry temp removed

--- lib/validators/attentry.d.ts
+++ lib/validators/attentry.d.ts
@@ -2,6 +2,5 @@ export declare const AttdnceEntrySchema: {
     docdate: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
     idcard: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     shift: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    temp: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     lop: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
--- src/validators/attentry.ts
+++ src/validators/attentry.ts
@@ -5,6 +5,6 @@ export const AttdnceEntrySchema = {
   docdate: yup.date().required(REQUIRED_TEXT).max("2500-00-00", "Invalid Date"),
   idcard: yup.string().required(REQUIRED_TEXT),
   shift: yup.string().required(REQUIRED_TEXT),
-  temp: yup.string().required(REQUIRED_TEXT),
+  // temp: yup.string().required(REQUIRED_TEXT),
   lop: yup.string().required(REQUIRED_TEXT),
 }
