a52ce2f1e7d5975c1919569a8eecd0fa4e401024
unknown <dhinesh.sivakumar@ekalaiv.com>
2022-09-16 15:22:32 +0530

    customer type validation updated

--- src/validators/customertype.ts
+++ src/validators/customertype.ts
@@ -6,6 +6,8 @@ import {ALPHA_TEXT} from '../config/validators'
 
 
 export const customertypeSchema = {
-  customer_type: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
-  cust_type_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+  // customer_type: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+  // cust_type_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+  customer_type: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT),
+  cust_type_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)),
  }
