c21c3c7708a5cdffe3427ee2d7c84bf261fa2f42
unknown <dhinesh.sivakumar@ekalaiv.com>
2022-09-16 15:39:46 +0530

    crm validation updated

--- src/validators/category.ts
+++ src/validators/category.ts
@@ -6,7 +6,9 @@ import {ALPHA_TEXT} from '../config/validators'
 
 
 export var categorySchema = {
-    category_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/, (ALPHA_TEXT)),
-    category_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+    // category_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/, (ALPHA_TEXT)),
+    // category_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+    category_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT),
+    category_description:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)),
    }
   
