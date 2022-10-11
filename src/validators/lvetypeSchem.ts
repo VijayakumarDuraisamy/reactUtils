bb309c1be1f532b3f8caaf32d1a65762a635e751
Gokulraj <gokulece888@yahoo.com>
2022-10-11 17:29:56 +0530

    lvetypeschema fixed

--- src/validators/lvetypeSchem.ts
+++ src/validators/lvetypeSchem.ts
@@ -2,6 +2,6 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 
 export const lvetypeSchema = {
-    leavetype_name: yup.string().required(REQUIRED_TEXT),
+    leavetype_name: yup.string().trim().matches(/[abcdefghijklmnopqrstuvwxyz]+/ , 'Invalid Name').required(REQUIRED_TEXT),
     //allow_override: yup.string().required(REQUIRED_TEXT)
 }
