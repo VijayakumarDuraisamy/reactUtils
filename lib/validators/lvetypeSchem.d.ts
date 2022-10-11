420e0adc01521022aa720ae3933fc1d271e006d2
viji G <vijayalakshmi.ganeshsundar@ekalaiv.com>
2022-10-11 22:01:05 +0530

    leave validation updated

--- lib/validators/lvetypeSchem.d.ts
+++ lib/validators/lvetypeSchem.d.ts
@@ -1,3 +1,4 @@
 export declare const lvetypeSchema: {
     leavetype_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    allocate_days: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
