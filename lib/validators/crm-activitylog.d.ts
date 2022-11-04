a5bb0ee5b7ef9faee514044d3bf450f13199a31a
Gokulraj <gokulece888@yahoo.com>
2022-11-04 16:07:44 +0530

    crm activity log updated

--- lib/validators/crm-activitylog.d.ts
+++ lib/validators/crm-activitylog.d.ts
@@ -2,5 +2,5 @@ export declare var activitylogSchema: {
     attended_by: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     action_taken: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     attended_date: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
-    time_spent: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
+    spen_time: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
 };
