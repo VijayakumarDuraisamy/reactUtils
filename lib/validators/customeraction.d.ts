e5a51ab7c47da0388b2980b9a0d769ef183b8281
unknown <dhinesh.sivakumar@ekalaiv.com>
2022-12-15 20:54:32 +0530

    stage, action levels removed

--- lib/validators/customeraction.d.ts
+++ lib/validators/customeraction.d.ts
@@ -1,6 +1,5 @@
 import * as yup from 'yup';
 export declare const customerActionSchema: {
-    action_level: import("yup/lib/number").RequiredNumberSchema<number | undefined, import("yup/lib/types").AnyObject>;
     action_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     action_description: yup.StringSchema<string | null | undefined, import("yup/lib/types").AnyObject, string | null | undefined>;
 };
