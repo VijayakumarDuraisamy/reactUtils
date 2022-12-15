dd3abed7cb646d9a8bd20d4494780f130c4c7128
unknown <dhinesh.sivakumar@ekalaiv.com>
2022-12-15 20:40:31 +0530

    crm postsale product added

---
+++ src/validators/crmproduct.ts
@@ -0,0 +1,11 @@
+import * as yup from 'yup'
+import { MINMAX_TEXT, NUM_TEXT, REQUIRED_TEXT } from '../config/validators'
+import {LIMIT_TEXT} from '../config/validators'
+import {DESCRIPTION_TEXT} from '../config/validators'
+import {ALPHA_TEXT} from '../config/validators'
+
+export const productSchema = {
+    sku_id: yup.string().trim().required(REQUIRED_TEXT),
+    sku_name: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+    prod_dimension:yup.string().trim().nullable(true).max(250,(DESCRIPTION_TEXT)).matches(/^[aA-zZ\s]+$/, (ALPHA_TEXT)),
+ }
