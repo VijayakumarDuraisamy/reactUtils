57633912a6ccf728512d7e254fe7c5b85db335b1
unknown <dhinesh.sivakumar@ekalaiv.com>
2022-12-19 15:29:15 +0530

    crm Product validation updated

--- lib/validators/crmproduct.d.ts
+++ lib/validators/crmproduct.d.ts
@@ -2,5 +2,5 @@ import * as yup from 'yup';
 export declare const productSchema: {
     sku_id: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     sku_name: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    prod_dimension: yup.StringSchema<string | null | undefined, import("yup/lib/types").AnyObject, string | null | undefined>;
+    custom_fields_1: yup.StringSchema<string | null | undefined, import("yup/lib/types").AnyObject, string | null | undefined>;
 };
