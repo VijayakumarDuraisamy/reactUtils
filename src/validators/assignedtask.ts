876e316250da8a1ea210c921a963bd738816b39d
Gokulraj <gokulece888@yahoo.com>
2022-12-22 15:11:30 +0530

    date validation updated

--- src/validators/assignedtask.ts
+++ src/validators/assignedtask.ts
@@ -7,7 +7,7 @@ import {ALPHA_TEXT} from '../config/validators'
 
 export var assignedtaskSchema = {
     from_date:yup.date().required(REQUIRED_TEXT).max("2500-00-00", "Invalid Date"),
-    to_date: yup.date().required(REQUIRED_TEXT).min(yup.ref("from_date"), "End date can't be before start date"),
+    to_date: yup.date().required(REQUIRED_TEXT).min(yup.ref("from_date"), "End date can't be before start date").max("2500-00-00", "Invalid Date"),
     task:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT),
     sub_task:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT),
     category:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT),
