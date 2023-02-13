e7685fd449f883a0c09ca5ecfb619da525c42ae7
Gokulraj <gokulece888@yahoo.com>
2023-02-13 17:30:08 +0530

    module_page validation added

---
+++ src/validators/pagemodules.ts
@@ -0,0 +1,9 @@
+import * as yup from 'yup'
+import { REQUIRED_TEXT } from '../config/validators'
+import {LIMIT_TEXT} from '../config/validators'
+import {ALPHA_TEXT} from '../config/validators'
+
+
+export const pageSchema = {
+  page_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+ }
