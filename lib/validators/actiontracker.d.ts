06d2ba89712989851c7199499f343a13e484115c
Gokulraj <gokulece888@yahoo.com>
2023-03-04 09:06:42 +0530

    action tracker utils updated

--- lib/validators/actiontracker.d.ts
+++ lib/validators/actiontracker.d.ts
@@ -1,6 +1,5 @@
 export declare var actiontrackerSchema: {
     item: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
-    start_date: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
     exp_end_date: import("yup/lib/date").RequiredDateSchema<Date | undefined, import("yup/lib/types").AnyObject>;
     assigned_to_tracker_id: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
     lead_status_id: import("yup/lib/string").RequiredStringSchema<string | undefined, import("yup/lib/types").AnyObject>;
