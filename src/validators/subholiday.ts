8491fff1140b628dde15b933e67bcaafc5c5d006
Gokulraj <gokulece888@yahoo.com>
2023-03-08 17:08:53 +0530

    holiday and subholiday validation

--- src/validators/subholiday.ts
+++ src/validators/subholiday.ts
@@ -1,8 +1,10 @@
 import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
+import {LIMIT_TEXT} from '../config/validators'
+import {ALPHA_TEXT} from '../config/validators'
 
 export const subholidaySchema = {
-  holidays: yup.string().matches(/[abcdefghijklmnopqrstuvwxyz]+/, 'Invalid Holiday Name').required(REQUIRED_TEXT),
+  holidays: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
   holidaydate: yup.date().required(REQUIRED_TEXT).max("2500-00-00", "Invalid Date"),
   holidayid: yup.string().required(REQUIRED_TEXT)
 }
