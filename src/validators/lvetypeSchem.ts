beb19280453e29cb107f755af650a9a0b278b621
Gokulraj <gokulece888@yahoo.com>
2023-03-16 18:25:24 +0530

    leave type schema updated

--- src/validators/lvetypeSchem.ts
+++ src/validators/lvetypeSchem.ts
@@ -4,6 +4,6 @@ import {LIMIT_TEXT} from '../config/validators'
 import {ALPHA_TEXT} from '../config/validators'
 
 export const lvetypeSchema = {
-    leavetype_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+    leavetype_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT),
     allocate_days: yup.string().trim().matches(/^[1-9]\d*$/, 'You must enter a positive number').required(REQUIRED_TEXT),
 }
