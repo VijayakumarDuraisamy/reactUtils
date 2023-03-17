19832f4aa691977124194624fb4b1f6a84ffd4bb
Gokulraj <gokulece888@yahoo.com>
2023-03-17 16:23:18 +0530

    holiday schemas updated

--- src/validators/holiday.ts
+++ src/validators/holiday.ts
@@ -4,5 +4,5 @@ import {LIMIT_TEXT} from '../config/validators'
 import {ALPHA_TEXT} from '../config/validators'
 
 export const holidaySchema = {
-  holiday_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+  holiday_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/[a-zA-Z&_\.-]/ , (ALPHA_TEXT)),
    }
--- src/validators/subholiday.ts
+++ src/validators/subholiday.ts
@@ -4,7 +4,7 @@ import {LIMIT_TEXT} from '../config/validators'
 import {ALPHA_TEXT} from '../config/validators'
 
 export const subholidaySchema = {
-  holidays: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+  holidays: yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/[a-zA-Z&_\.-]/ , (ALPHA_TEXT)),
   holidaydate: yup.date().required(REQUIRED_TEXT).max("2500-00-00", "Invalid Date"),
   holidayid: yup.string().required(REQUIRED_TEXT)
 }
