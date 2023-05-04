6ba3e74e9c8ffab7a9b61fca35c29a3e8e4444b3
Gokulraj <gokulece888@yahoo.com>
2023-05-04 20:16:57 +0530

    type validation updated

--- src/validators/category_type.ts
+++ src/validators/category_type.ts
@@ -5,5 +5,5 @@ import {ALPHA_TEXT} from '../config/validators'
 
 
 export const categoryTypeSchema = {
-    sub_category_name:yup.string().trim().max(50,(LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[aA-zZ\s]+$/ , (ALPHA_TEXT)),
+    sub_category_name: yup.string().trim().max(50, (LIMIT_TEXT)).required(REQUIRED_TEXT).matches(/^[a-z][a-z0-9_]*$/, "Enter a Valid Type Name"),
  }
