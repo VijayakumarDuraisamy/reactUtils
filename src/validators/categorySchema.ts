eba23bfe5d14204b2f3641103bdb46e09ff763ca
Gokulraj <gokulece888@yahoo.com>
2023-06-20 14:59:43 +0530

    category schema updated for asset team

--- src/validators/categorySchema.ts
+++ src/validators/categorySchema.ts
@@ -2,6 +2,6 @@ import * as yup from 'yup'
 import { REQUIRED_TEXT } from '../config/validators'
 //for Skill Management
 export const categorySchema = {
-  category_name: yup.string().trim().required(REQUIRED_TEXT),
+  category_name: yup.string().matches(/^[a-zA-Z0-9\-]+(\s[a-zA-Z0-9\-]+)*$/, "Enter a Valid Category name").trim().required(REQUIRED_TEXT),
   //category_description: yup.string().required(REQUIRED_TEXT)
   }
