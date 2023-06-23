f304085beea1ba588fcb3cc29f18656bcb4d16d0
JayasuryaVelusamy <jayasurya.velusamy@ekalaiv.com>
2023-06-23 17:06:35 +0530

    With agrird Customization

--- lib/components/Aggrid.d.ts
+++ lib/components/Aggrid.d.ts
@@ -1,5 +1,15 @@
 export default AgGrid;
-declare function AgGrid({ columnDefs, rowData }: {
+declare function AgGrid({ columnDefs, rowData, pagination, paginationPageSize, headerHeight, onGridReady, defaultColDef, rowSelection, onFilterChanged, checkboxSelection, headerCheckboxSelectionFilteredOnly, headerCheckboxSelection }: {
     columnDefs: any;
     rowData: any;
+    pagination: any;
+    paginationPageSize: any;
+    headerHeight: any;
+    onGridReady: any;
+    defaultColDef: any;
+    rowSelection: any;
+    onFilterChanged: any;
+    checkboxSelection: any;
+    headerCheckboxSelectionFilteredOnly: any;
+    headerCheckboxSelection: any;
 }): JSX.Element;
--- src/components/Aggrid.js
+++ src/components/Aggrid.js
@@ -1,13 +1,44 @@
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-const AgGrid = ({ columnDefs, rowData }) => {
-    return (
-      <div className="ag-theme-alpine" style={{ height: '300px', width: '100%' }}>
-        <AgGridReact columnDefs={columnDefs} rowData={rowData} />
-      </div>
-    );
-  };
-  
-  export default AgGrid;
-  
+const AgGrid = ({
+  columnDefs,
+  rowData,
+   pagination, 
+   paginationPageSize, 
+   headerHeight, 
+   onGridReady, 
+   defaultColDef, 
+   rowSelection, 
+   onFilterChanged, 
+   checkboxSelection, 
+   headerCheckboxSelectionFilteredOnly,
+   headerCheckboxSelection
+
+   }) => {
+  return (
+    <div className="ag-theme-alpine" >
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowData={rowData}
+        pagination={pagination}
+        paginationPageSize={paginationPageSize}
+        headerCheckboxSelection={headerCheckboxSelection} // Enable checkbox selection in the header
+        headerCheckboxSelectionFilteredOnly={headerCheckboxSelectionFilteredOnly} // Enable checkbox selection only for filtered rows in the header
+        checkboxSelection={checkboxSelection} // Enable checkbox selection for each row
+        onFilterChanged={onFilterChanged} // Callback function for filter changes
+        rowSelection={rowSelection} // Set the row selection mode
+        // rowData={AGData} // Set the data for the grid
+        // columnDefs={AgColumnData} // Set the column definitions for the grid
+        defaultColDef={defaultColDef} // Set the default column definitions for the grid
+        // pagination={true} // Enable pagination
+        onGridReady={onGridReady} // Callback function for grid ready event
+        // paginationPageSize={100} // Set the number of rows per page in pagination
+        headerHeight={headerHeight} // Set the height of the header
+
+      />
+    </div>
+  );
+};
+
+export default AgGrid;
