7bebe403efd507dcc906b8a7b5701ffd2a499e26
JayasuryaVelusamy <jayasurya.velusamy@ekalaiv.com>
2023-06-26 16:18:29 +0530

    Version 3.0.10 - Textfield, Button, Autocomplete, Snackbar and Icons as compentents

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import './mui.css';
+// import './mui.css';
 const GlobalButton = ({ children, ...props }) => {
   return <Button {...props}>{children}</Button>;
 };
